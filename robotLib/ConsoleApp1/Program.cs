69e5ad8ac193df5fd84cc64415e20aa80678281b
ulshinvv <158988350+ulshinvv@users.noreply.github.com>
2024-02-12 14:00:13 +0700

    Add files via upload

---
+++ robotLib/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using RobotLibrary;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot1 = new Robot("Робот 1", 200, 199);
+            Robot robot2 = new Robot("Робот 2", 200, 199);
+
+            
+
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                robot1.Move();
+                robot2.Move();
+
+                Console.WriteLine(robot1.GetInfo());
+                Console.WriteLine(robot2.GetInfo());
+                Console.WriteLine("");
+
+                Thread.Sleep(2000); 
+            }
+        }
+    }
+}
---
+++ robotLib/RobotLibrary/Robot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RobotLibrary
+{
+    public class Robot
+    {
+        public string Name { get; private set; }
+        public Tuple<int, int> Position { get; private set; }
+
+        private Thread moveThread;
+
+        private string directions2 = "";
+
+        public Robot(string name, int x, int y)
+        {
+            Name = name;
+            Position = Tuple.Create(x, y);
+            moveThread = new Thread(Move);
+        }
+
+        public void StartMoving()
+        {
+            moveThread.Start();
+        }
+
+        public void Move()
+        {
+            Random random = new Random();
+            int direction = random.Next(4);
+            
+            switch (direction)
+            {
+                case 0:
+                    Position = Tuple.Create(Position.Item1, Position.Item2 + 1);
+                    directions2 = "вперед";
+                    break;
+                case 1:
+                    Position = Tuple.Create(Position.Item1, Position.Item2 - 1);
+                    directions2 = "назад";
+                    break;
+                case 2:
+                    Position = Tuple.Create(Position.Item1 - 1, Position.Item2);
+                    directions2 = "влево";
+                    break;
+                case 3:
+                    Position = Tuple.Create(Position.Item1 + 1, Position.Item2);
+                    directions2 = "вправо";
+                    break;
+            }
+            Thread.Sleep(1000);
+        }
+
+        public string GetInfo()
+        {
+            
+            return $"{Name} {directions2} ({Position.Item1}, {Position.Item2})";
+        }
+    }
+}
